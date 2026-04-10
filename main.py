20097dad034131e8fab65c532099b3c643584435
Sanaya James <sanaya.j.james@gmail.com>
2026-04-10 14:39:01 -0700

    completed lab. implemented queries for all steps

--- main.py
+++ main.py
@@ -4,34 +4,52 @@ import pandas as pd
 conn = sqlite3.connect('data.sqlite')
 # cur = conn.pd
 
-# STEP 2
-# Replace None with your code
-df_first_five = None
+df_first_five = pd.read_sql("""
+                            SELECT employeeNumber, lastName FROM employees;
+                            """, conn)
 
-# STEP 3
-# Replace None with your code
-df_five_reverse = None
+df_five_reverse = pd.read_sql("""
+                            SELECT lastName, employeeNumber FROM employees;
+                            """, conn)
 
-# STEP 4
-# Replace None with your code
-df_alias = None
+df_alias = pd.read_sql("""
+                            SELECT lastName, employeeNumber AS ID FROM employees;
+                            """, conn)
 
-# STEP 5
-# Replace None with your code
-df_executive = None
+df_executive = pd.read_sql("""
+                           SELECT
+                           CASE
+                           WHEN jobTitle = "President" THEN "Executive"
+                           WHEN jobTitle = "VP Sales" THEN "Executive"
+                           WHEN jobTitle = "VP Marketing" THEN "Executive"
+                           ELSE "Not executive"
+                           END AS role
+                           FROM employees;
+                           """, conn)
 
-# STEP 6
-# Replace None with your code
-df_name_length = None
+df_name_length = pd.read_sql("""
+                             SELECT length(lastName) AS name_length
+                             FROM employees;
+                             """, conn)
 
-# STEP 7
-# Replace None with your code
-df_short_title = None
+df_short_title = pd.read_sql("""
+                             SELECT substr(jobTitle, 1, 2) AS short_title
+                             FROM employees;
+                             """, conn)
 
-# STEP 8
-# Replace None with your code
-sum_total_price = None
+sum_total_price = pd.read_sql("""
+                              SELECT SUM(ROUND(priceEach * quantityOrdered)) AS total_price
+                              FROM orderDetails;
+                              """, conn).values[0]
 
-# STEP 9
-# Replace None with your code
-df_day_month_year = None
+print(sum_total_price)
+
+df_day_month_year = pd.read_sql("""
+                                SELECT orderDate,
+                                substr(orderDate, 9, 10) AS day,
+                                substr(orderDate, 6, 6) AS month,
+                                substr(orderDate, 1, 4) AS year
+                                FROM orders;
+                                """, conn)
+
+print(df_day_month_year)
